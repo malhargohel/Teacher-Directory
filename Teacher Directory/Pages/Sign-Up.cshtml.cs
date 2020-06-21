b67a71c8c5e8664d1a7fc108f4b6d1c0392ecf08
Malhar Gohel <ac98824@avcol.school.nz>
2020-06-22 10:40:39 +1200

    Yes

---
+++ Teacher Directory/Pages/Sign-Up.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Teacher_Directory.Pages
+{
+    public class SignUpModel : PageModel
+    {
+        private readonly ILogger<SignUpModel> _logger;
+
+        public SignUpModel(ILogger<SignUpModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
